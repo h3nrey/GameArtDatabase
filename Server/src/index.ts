71eb9b82a6480a7a10ea65a6fb43d5db3be91bd4
h3nrey <pedro.novaes.education@gmail.com>
2022-11-26 15:38:41 -0300

    test: Changing the home to the root route

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -10,26 +10,25 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/home", async (req, res) => {
-  const user = await prisma.user.findMany()
+app.get("/", async (req, res) => {
+  const user = await prisma.user.findMany({})
   // const user = {name: "geroge", age: 12}
 
   res.json(user).status(201);
 });
 
 
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Todo REST API</h1>
-  <h2>Available Routes</h2>
-  <pre>
-    GET, POST /todos
-    GET, PUT, DELETE /todos/:id
-  </pre>
-  `.trim(),
-  );
-});
+// app.get("/", async (req, res) => {
+//   res.send(
+//     `
+//   <h1>Todo REST API</h1>
+//   <h2>Available Routes</h2>
+//   <pre>
+//     GET, POST /home
+//   </pre>
+//   `.trim(),
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
