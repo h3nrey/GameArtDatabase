7ddef72d830027f92e18a210a4ace27ee825c780
h3nrey <pedro.novaes.education@gmail.com>
2022-11-26 15:50:50 -0300

    test: removing status on response

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -11,24 +11,24 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const user = await prisma.user.findMany({})
+  const user = await prisma.user.findMany()
   // const user = {name: "geroge", age: 12}
 
-  res.json(user).status(201);
+  res.json(user);
 });
 
 
-// app.get("/", async (req, res) => {
-//   res.send(
-//     `
-//   <h1>Todo REST API</h1>
-//   <h2>Available Routes</h2>
-//   <pre>
-//     GET, POST /home
-//   </pre>
-//   `.trim(),
-//   );
-// });
+app.get("/", async (req, res) => {
+  res.send(
+    `
+  <h1>Todo REST API</h1>
+  <h2>Available Routes</h2>
+  <pre>
+    GET, POST /home
+  </pre>
+  `.trim(),
+  );
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
