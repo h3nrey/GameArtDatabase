b8dbcf734991a08497db69b9bea15b10ff0eb096
h3nrey <pedro.novaes.education@gmail.com>
2022-11-26 14:56:23 -0300

    feat: initial setup of project

---
+++ Server/src/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
+
+app.get("/home", async (req, res) => {
+  const user = await prisma.user.findMany()
+
+  res.json(user).status(201);
+});
+
+
+app.get("/", async (req, res) => {
+  res.send(
+    `
+  <h1>Todo REST API</h1>
+  <h2>Available Routes</h2>
+  <pre>
+    GET, POST /todos
+    GET, PUT, DELETE /todos/:id
+  </pre>
+  `.trim(),
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
